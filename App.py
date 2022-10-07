028b49af8d5e42c573579bb2ee56de9e6be9349e
ashiqueam <114980303+ashiqueam@users.noreply.github.com>
2022-10-07 12:12:39 +0530

    Create app.py

---
+++ App.py
@@ -0,0 +1,18 @@
+s1-input()
+s2-input()
+
+ref="flames"
+
+g=0
+
+for i in s1:
+    if i in 52:
+        s1=s1.replace(i,"") 
+        s2=s2.replace(i,"")
+
+sum-len(s1)+len(s2)
+
+while len(ref)!-1: 
+    q=(q+sum-1)% len(ref) 
+    ref-ref.replace(ref [q],"") 
+print (ref)
